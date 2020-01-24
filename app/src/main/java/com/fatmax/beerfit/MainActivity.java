bff94597cbc52d1c1a6219bbaa21fd43bef3883f
Max Saperstone <max.saperstone@coveros.com>
2020-01-24 17:27:57 -0500

    Adding initial sqlite setup

--- app/src/main/java/com/fatmax/beerfit/MainActivity.java
+++ app/src/main/java/com/fatmax/beerfit/MainActivity.java
@@ -1,5 +1,7 @@
 package com.fatmax.beerfit;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -8,6 +10,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteDatabase database;
 
     TextView beerCounter;
     ImageButton drankBeer;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         beerCounter = findViewById(R.id.beersLeft);
         drankBeer = findViewById(R.id.drankABeer);
 
+        setupDatabase();
         // TODO - need to fix this to pull in total beers (will need log, etc)
         beersRemaining = 10;
         //on app launch, set beer to 10
@@ -34,5 +39,29 @@ public class MainActivity extends AppCompatActivity {
                 beerCounter.setText(String.valueOf(beersRemaining));
             }
         });
+    }
+
+    private void setupDatabase() {
+        database = openOrCreateDatabase("beerfit", MODE_PRIVATE, null);
+        if (!isTableExists("Measurement")) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS Measurement(id INTEGER PRIMARY KEY AUTOINCREMENT, type VARCHAR);");
+            database.execSQL("INSERT INTO Measurement VALUES(null,'time');");
+            database.execSQL("INSERT INTO Measurement VALUES(null,'distance');");
+        }
+        if (!isTableExists("Goals")) {
+            database.execSQL("CREATE TABLE IF NOT EXISTS Goals(id INTEGER PRIMARY KEY AUTOINCREMENT, type VARCHAR,Activity VARCHAR,Measurement INTEGER,Amount INTEGER);");
+        }
+    }
+
+    private boolean isTableExists(String tableName) {
+        boolean isExist = false;
+        Cursor cursor = database.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '" + tableName + "'", null);
+        if (cursor != null) {
+            if (cursor.getCount() > 0) {
+                isExist = true;
+            }
+            cursor.close();
+        }
+        return isExist;
     }
 }
