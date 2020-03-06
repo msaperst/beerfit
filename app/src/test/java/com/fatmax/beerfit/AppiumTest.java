5fdcd0d7e9a3a0c79b9f19143801e9c3d0b42519
Max Saperstone <msaperst@gmail.com>
2020-03-05 22:18:13 -0500

    Adding initial functional test

---
+++ app/src/test/java/com/fatmax/beerfit/AppiumTest.java
@@ -0,0 +1,47 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
+import static org.junit.Assert.assertEquals;
+
+public class AppiumTest {
+    private AndroidDriver driver;
+    private AppiumDriverLocalService service = AppiumDriverLocalService.buildService(new AppiumServiceBuilder().usingAnyFreePort());
+
+    @Before
+    public void setupDriver() throws MalformedURLException {
+        service.start();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability("appPackage", "com.fatmax.beerfit");
+        capabilities.setCapability("appActivity", "MainActivity");
+//        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(service, capabilities);
+    }
+
+    @After
+    public void tearDownDriver() {
+        driver.quit();
+        service.stop();
+    }
+
+    @Test
+    public void drinkABeer() {
+        int beers = Integer.parseInt(driver.findElement(By.id("beersLeft")).getText());
+        driver.findElement(By.id("drankABeer")).click();
+        assertEquals(beers-1, Integer.parseInt(driver.findElement(By.id("beersLeft")).getText()));
+    }
+
+}
